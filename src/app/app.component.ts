ff0cbb9e0a58e97f318eacf67051e9a7de5ecad7
ldimuro <ldimuro@asu.edu>
2023-06-12 16:29:40 -0700

    added simulation data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Song } from './song-model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'bucket-setlist';
+  total_songs: Song[] = [];
+
+  constructor(
+    private httpClient: HttpClient
+  ){}
+
+  async ngOnInit(): Promise<void> {
+    const songJSONData= await this.httpClient.get('assets/spotify-simulation-data.json', { responseType: 'json' }).toPromise();
+    const songs: Song[] = songJSONData['data'];
+    this.total_songs = songs;
+
+    console.log('DATA: ', songs);
+    
+  }
+
+
+
+  
+
+
+
+
 }
---
+++ src/app/song-model.ts
@@ -0,0 +1,7 @@
+export class Song {
+    song_name: string;
+    artist: string;
+    album: string;
+    cover_art: any;
+    song_length: number;
+}
