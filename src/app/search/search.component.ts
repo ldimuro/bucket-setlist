c48d0bb5ff9b31b7095fffb652bbaf3cd2adced7
ldimuro <ldimuro@asu.edu>
2023-06-23 14:02:09 -0700

    user can now select a song in the 'Search' page and get redirected to 'Home' page

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Song } from '../song-model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -10,9 +11,12 @@ import { Song } from '../song-model';
 export class SearchComponent implements OnInit {
 
   total_songs: Song[] = [];
+  confirmation_modal_open = false;
+  selected_song;
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private router: Router
   ) { }
 
   async ngOnInit() {
@@ -21,6 +25,27 @@ export class SearchComponent implements OnInit {
     this.total_songs = songs;
 
     console.log('DATA: ', songs);
+  }
+
+  songClicked(song: any) {
+    console.log('SONG CLICKED: ', song);
+    this.selected_song = song;
+    this.confirmation_modal_open = true;
+
+    document.getElementById(`searchComponent`).classList.add('blur-background_in');
+  }
+
+  // If user accepts the song, take them to main Bucket Setlist page
+  confirmSong() {
+    document.getElementById(`searchComponent`).classList.remove('blur-background_in');
+    this.router.navigate(['/home']);
+
+  }
+
+  cancel() {
+    this.selected_song = null;
+    this.confirmation_modal_open = false;
+    document.getElementById(`searchComponent`).classList.remove('blur-background_in');
   }
 
 }
