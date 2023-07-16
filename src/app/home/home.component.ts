26fece7457630ebcb5b09dd01c900e4a22ae993f
Speedylevis <caden.matt.keller@gmail.com>
2023-07-16 10:39:13 -0500

    refresh token cycle is set, but there is still a delay in the web player showing the most recently added song

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,17 +28,29 @@ export class HomeComponent implements OnInit, OnDestroy {
           this.chosen_track = val;
           console.log('CHOSEN TRACK: ', this.chosen_track);
 
+          /*
+            This is kind of like a paradox because firebase is constantly cycling and replacing the old refresh token with the new one, but there has to be an original token.
+            So this is what I did and what we'll have to do if the cycle breaks:
+            1) Comment out the firebase get/set
+            2) Log into the bucket setlist web app as Clou Diller to have Spotify generate a new refresh_token
+            3) Uncomment the firebase setter
+            3) Have Spotify generate a new refresh token for Clou Diller (we can do this by having Clou make any kind of call to the api) and it will be stored in firebase
+            4) Uncomment the firebase getter and now the cycle should be set
+          */
+
+
           let storeRefreshToken;
           await this.firebaseSvc.getRefreshToken().then(data => {
             console.log('GOT REFRESH TOKEN: ', data);
             storeRefreshToken = data;
           });
-
           const { access_token, refresh_token } = await this.spotifySvc.refreshAccessToken(storeRefreshToken);
+
+          console.log("New Refresh Token: " + refresh_token);
 
           await this.firebaseSvc.updateRefreshToken(refresh_token);
 
-          this.addSongToPlaylist(access_token, this.chosen_track.id);
+          await this.addSongToPlaylist(access_token, this.chosen_track.id);
         }
     });
 
--- src/app/spotify/spotify.service.ts
+++ src/app/spotify/spotify.service.ts
@@ -27,10 +27,6 @@ export class SpotifyService {
     //if the user is already logged in the search bar will have the code in the URL
     else {
       const accessToken = await getAccessToken(clientId, code);
-      
-      //use this code to print the refresh token to the console, make sure to edit method return as well
-      //const accessToken = await getAccessToken(clientId, code);
-      //console.log("RefreshToken: " + accessToken);
 
       this.setAccessToken(accessToken);
 
@@ -63,14 +59,10 @@ export class SpotifyService {
   }
 
   async refreshAccessToken(currentRefreshToken: string) {
-    var refreshToken = "AQBbxn5cqhNGRgZLI_y6uoMiwKmie6zttxCH5x34pwmAjVoI_wh-tPcnOUCRv9se5kvepqIE0ek89vN1lUl995w1DXSeozHilEqSffp6NImMz0zwTTMfn9vEUDRbs8MvulE";
-
-    if(currentRefreshToken != refreshToken)
-    { refreshToken = currentRefreshToken; }
-
+  
     const params = new URLSearchParams();
     params.append("grant_type", "refresh_token");
-    params.append("refresh_token", refreshToken);
+    params.append("refresh_token", currentRefreshToken);
     params.append("client_id", "3d2321a8c72646e191c8145193fa1cf7");
   
     const result = await fetch("https://accounts.spotify.com/api/token", {
@@ -214,12 +206,9 @@ export async function getAccessToken(clientId: string, code: string): Promise<st
     body: params
   });
 
-  //Swap the following lines of code to get the refresh token instead of the access token
-
-  const { access_token } = await result.json();
+  const { access_token, refresh_token } = await result.json();
+  console.log("Refresh Token: " + refresh_token);
   return access_token;
-  //const { refresh_token } = await result.json();
-  //return refresh_token;
   
 }
 
